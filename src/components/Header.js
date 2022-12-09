8768a28c5e9fd0bcbee32bed8eb505d7275fc101
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-12-08 19:14:40 -0800

    added burger menu icon from google fonts sans positioning

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,19 @@
 import Container from "react-bootstrap/Container";
 
-function Header(){
-    return(
-        <div className="header">
-            <Container>
-                <div className='logo'>
-                    <h1>Rob The Plumber</h1>
-                    <p className="tagline">Plumbing and Maintenance</p>
-                </div>
-            </Container>
+function Header() {
+  return (
+    <div className="header">
+      <Container>
+        <div className="logo">
+          <h1>Rob The Plumber</h1>
+          <p className="tagline">Plumbing and Maintenance</p>
         </div>
-    )
+        <div className="navigation">
+          <span class="material-symbols-outlined">menu</span>
+        </div>
+      </Container>
+    </div>
+  );
 }
 
 export default Header;
