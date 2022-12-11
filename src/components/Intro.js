7e652c79ab8e1fb71570bb31b0d7c912b23820a1
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-12-10 21:06:57 -0800

    added contact ribbon to intro component

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,6 +1,12 @@
 function Intro() {
     return (
+      
       <div className="intro">
+        <div id='home'></div>
+        <div className="ribbon">
+          <p>Proudly Serving Nanaimo, BC</p>
+          <p>555-555-5555</p>
+        </div>
         <h1>Intro</h1>
         <ul>
             <li>Value Proposition (8 words max)</li>
