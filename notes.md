0be2f2ad9cf3a019df00c6426cdd76190011c9f0
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-12-12 15:42:13 -0800

    added black pipe image and logo for intro component

--- notes.md
+++ notes.md
@@ -1,3 +1,5 @@
 First crack at animated burger menu icon from [this video](https://www.youtube.com/watch?v=dIyVTjJAkLw)
 
 Had to refactor for react by watching [this video](https://www.youtube.com/watch?v=XLHdF7z77YU)
+
+Read [this blog](https://j.eremy.net/confused-about-rem-and-em/) about REM and EM
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,4 +1,6 @@
 import phoneIcon from '../images/phone-icon.png'
+import blackPipe from '../images/black-pipe.png';
+import Container from "react-bootstrap/Container";
 
 function Intro() {
     return (
@@ -9,11 +11,15 @@ function Intro() {
           <p>Proudly Serving Nanaimo, BC</p>
           <p><img className='ribbon-image' src={phoneIcon} alt='Plumber Nanaimo Honest' /> 555-555-5555</p>
         </div>
-        <h1>Intro</h1>
-        <ul>
-            <li>Value Proposition (8 words max)</li>
-            <li>Visual Focus</li>
-        </ul>
+        <Container>
+        <div className='intro-heading'>
+          <img src={blackPipe} alt='Black Pipe'/>
+          <div className='intro-logo'>
+            <h1 className='intro-logo-heading'>Rob The Plumber</h1>
+            <p className="tagline">Plumbing and Maintenance</p>
+          </div>
+        </div>
+        </Container>
       </div>
     );
   }
