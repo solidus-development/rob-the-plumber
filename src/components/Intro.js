64087a8f6f2e452adb27180986eee7ddb4bd93eb
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-12-13 19:02:55 -0800

    added black pipe and intro logo to Intro component, coded for responsiveness, added background img to cta

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,6 +1,5 @@
 import phoneIcon from '../images/phone-icon.png'
-import blackPipe from '../images/black-pipe.png';
-import Container from "react-bootstrap/Container";
+import blackPipe from '../images/black-pipe350x349.png';
 
 function Intro() {
     return (
@@ -9,17 +8,21 @@ function Intro() {
         <div id='home'></div>
         <div className="ribbon">
           <p>Proudly Serving Nanaimo, BC</p>
-          <p><img className='ribbon-image' src={phoneIcon} alt='Plumber Nanaimo Honest' /> 555-555-5555</p>
+          <p><img className='ribbon-image' src={phoneIcon} alt='Plumber Nanaimo Honest' />555-555-5555</p>
         </div>
-        <Container>
-        <div className='intro-heading'>
-          <img src={blackPipe} alt='Black Pipe'/>
-          <div className='intro-logo'>
-            <h1 className='intro-logo-heading'>Rob The Plumber</h1>
-            <p className="tagline">Plumbing and Maintenance</p>
-          </div>
+        <div className='intro-billboard'>
+              <div className='intro-billboard-content-container'>
+                <img className='black-pipe' src={blackPipe} alt='Black Pipe'/>
+                <div className='intro-logo'>
+                  <h1 className='intro-logo-heading'>Rob The Plumber</h1>
+                  <p className="tagline">Plumbing and Maintenance</p>
+                </div>
+              </div>
         </div>
-        </Container>
+        <div className='cta'>
+          <img src='' alt='rob-the-plumber' />
+          
+        </div>
       </div>
     );
   }
